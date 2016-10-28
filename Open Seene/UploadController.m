a1dd124cfe7a343557fa6229d21f06fd7a628e4d
Mathias Zettler <zettler@zitcon.de>
2016-10-28 16:15:51 +0200

    experimenting with conserving the metadata of an image

--- Open Seene/UploadController.m
+++ Open Seene/UploadController.m
@@ -7,6 +7,8 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AssetsLibrary/AssetsLibrary.h>
+#import <ImageIO/ImageIO.h>
 #import "UploadController.h"
 #import "FlickrAPI.h"
 
@@ -25,8 +27,52 @@
     picker.allowsEditing = YES;
     picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
     
-    [self presentViewController:picker animated:YES completion:NULL];
+    // Retrieve the most recent image from PhotoLibrary
+    ALAssetsLibrary *assetsLibrary = [[ALAssetsLibrary alloc] init];
+    [assetsLibrary enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+        if (nil != group) {
+            // be sure to filter the group so you only get photos
+             [group setAssetsFilter:[ALAssetsFilter allPhotos]];
+             
+             if (group.numberOfAssets > 0) {
+                 [group enumerateAssetsAtIndexes:[NSIndexSet indexSetWithIndex:group.numberOfAssets - 1] options:0 usingBlock:^(ALAsset *result, NSUInteger index, BOOL *stop) {
+                      if (nil != result) {
+                          ALAssetRepresentation *repr = [result defaultRepresentation];
+                          // this is the most recent saved photo
+                          UIImage *photo = [UIImage imageWithCGImage:[repr fullResolutionImage]];
+                          [self checkSelectedPhoto: photo];
+                          self.imageView.image = photo;
+                          
+                          NSDictionary *metadata = repr.metadata;
+                          
+                          NSLog(@"image data %@", metadata);
+                          
+                          // we only need the first (most recent) photo -- stop the enumeration
+                          *stop = YES;
+                      }
+                  }];
+             }
+         }
+         *stop = NO;
+     } failureBlock:^(NSError *error) {
+         NSLog(@"error: %@", error);
+     }];
+    
+    
+    
+    //[self presentViewController:picker animated:YES completion:NULL];
 }
+
+- (Boolean)checkSelectedPhoto:(UIImage*)photo {
+    //UIImage *photo = self.imageView.image;
+    CGDataProviderRef provider = CGImageGetDataProvider(photo.CGImage);
+    NSData* data = (id)CFBridgingRelease(CGDataProviderCopyData(provider));
+    const uint8_t* bytes = [data bytes];
+
+    NSLog(@"image data %s", bytes);
+    return NO;
+}
+
 
 
 
